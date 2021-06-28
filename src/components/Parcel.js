0a82d5933e778fcbe6a81f0e2fd8aee7ac46bbba
Kevin Potschien <kpotschien@gmail.com>
2021-06-28 09:49:11 +0200

    merged first design and first logic

--- src/components/Parcel.js
+++ src/components/Parcel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import dhlLogo from '../images/dhl-brands.svg';
 
 const Parcel = ({ shipNr }) => {
 	const [info, setInfo] = useState([]);
@@ -24,7 +25,31 @@ const Parcel = ({ shipNr }) => {
 			});
 	}, []);
 
-	return <div>{info ? 'yes' : 'no'}</div>;
+	return (
+		<div className='card'>
+			<span className='hide'></span>
+			<div className='card-row'>
+				<h3>DHL Shipment</h3>
+				<img src={dhlLogo} className='provider-logo' alt='Provider Logo' />
+			</div>
+			<div className='card-row'>
+				<span>Tracking number: </span>
+				<span>{shipNr}</span>
+			</div>
+			<div className='card-row'>
+				<span>Destination: </span>
+				<span>{info ? 'yes' : 'no'}</span>
+			</div>
+			<div className='card-row'>
+				<span>Current location: </span>
+				<span>Kitzingen, Germany</span>
+			</div>
+			<div className='card-row'>
+				<span>Expected delivery: </span>
+				<span>Date</span>
+			</div>
+		</div>
+	);
 };
 
 export default Parcel;
