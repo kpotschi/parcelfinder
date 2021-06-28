1726b21a2ea3b5fe2d1247677627ddf340d8c44d
Melanie <melanie.schwarke@gmail.com>
2021-06-28 14:50:37 +0200

    adds media query for mobile view

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import './App.css';
-import logo from './images/shipping-fast-solid.svg';
+import "./App.css";
+import logo from "./images/shipping-fast-solid.svg";
 
-import Parcel from './components/Parcel';
+import Parcel from "./components/Parcel";
 
 function App() {
-	// check localstorage for data?
+  // check localstorage for data?
 
-	// save back to localstorage if new parcel
+  // save back to localstorage if new parcel
 
-	const ships = [
-		{ shipNr: '142775112730', delService: 'DHL' },
-		{ shipNr: '4381443301', delService: 'DHL' },
-	];
+  const ships = [
+    { shipNr: "142775112730", delService: "DHL" },
+    { shipNr: "4381443301", delService: "DHL" },
+  ];
 
-	return (
-		<div className='App'>
-			<h1 className='header'>
-				<span className='strong'>Parcel</span>finder
-				<img src={logo} className='logo' alt='Parcelfinder Logo'></img>
-			</h1>
-			<form className='form'>
-				<input
-					type='text'
-					className='input'
-					placeholder='Enter shipment number here'
-				/>
-				<button type='submit' className='submit'>
-					Search
-				</button>
-			</form>
-			<div className='card-container'>
-				{ships.map((item, index) => {
-					return <Parcel key={index} shipNr={item.shipNr} />;
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1 className="header">
+        <span className="strong">Parcel</span>finder
+        <img src={logo} className="logo" alt="Parcelfinder Logo"></img>
+      </h1>
+      <form className="form">
+        <input
+          type="text"
+          className="input"
+          placeholder="Enter shipment number here"
+        />
+        <button type="submit" className="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-container">
+        {ships.map((item, index) => {
+          return <Parcel key={index} shipNr={item.shipNr} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
