29beafab103f3a7024b99b51b6346b22c29a9caa
Melanie <melanie.schwarke@gmail.com>
2021-06-29 09:12:12 +0200

    adds the X

--- src/components/Parcel.js
+++ src/components/Parcel.js
@@ -3,9 +3,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import dhlLogo from "../images/dhl-brands.svg";
 
-
 const Parcel = ({ shipNr, eraseHandler }) => {
-	const [info, setInfo] = useState([]);
+  const [info, setInfo] = useState([]);
 
   const options = {
     method: "GET",
@@ -14,45 +13,46 @@ const Parcel = ({ shipNr, eraseHandler }) => {
     headers: { "DHL-API-Key": `${process.env.REACT_APP_DHL_API_KEY}` },
   };
 
-	useEffect(() => {
-		axios
-			.request(options)
-			.then(function (response) {
-				const allData = response.data.shipments;
-				setInfo(allData);
-			})
-			.catch(function (error) {
-				console.error(error);
-			});
-		//eslint-disable-next-line
-	}, []);
+  useEffect(() => {
+    axios
+      .request(options)
+      .then(function (response) {
+        const allData = response.data.shipments;
+        setInfo(allData);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    //eslint-disable-next-line
+  }, []);
 
-	return (
-		<div className='card' id={shipNr}>
-			<span className='hide' onClick={eraseHandler}></span>
-			<div className='card-row'>
-				<h3>DHL Shipment</h3>
-				<img src={dhlLogo} className='provider-logo' alt='Provider Logo' />
-			</div>
-			<div className='card-row'>
-				<span>Tracking number: </span>
-				<span>{shipNr}</span>
-			</div>
-			<div className='card-row'>
-				<span>Destination: </span>
-				<span>{info[0]?.events[0].description}</span>
-			</div>
-			<div className='card-row'>
-				<span>Current location: </span>
-				<span>Kitzingen, Germany</span>
-			</div>
-			<div className='card-row'>
-				<span>Expected delivery: </span>
-				<span>Date</span>
-			</div>
-		</div>
-	);
-
+  return (
+    <div className="card" id={shipNr}>
+      <span className="hide" onClick={eraseHandler}>
+        X
+      </span>
+      <div className="card-row">
+        <h3>DHL Shipment</h3>
+        <img src={dhlLogo} className="provider-logo" alt="Provider Logo" />
+      </div>
+      <div className="card-row">
+        <span>Tracking number: </span>
+        <span>{shipNr}</span>
+      </div>
+      <div className="card-row">
+        <span>Destination: </span>
+        <span>{info[0]?.events[0].description}</span>
+      </div>
+      <div className="card-row">
+        <span>Current location: </span>
+        <span>Kitzingen, Germany</span>
+      </div>
+      <div className="card-row">
+        <span>Expected delivery: </span>
+        <span>Date</span>
+      </div>
+    </div>
+  );
 };
 
 export default Parcel;
